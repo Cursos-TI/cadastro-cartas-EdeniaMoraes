a5c72e64a4691e403f8708100c8fe2c2a616dfdc
Edenia Moraes <161172018+EdeniaMoraes@users.noreply.github.com>
2025-11-26 22:13:10 +0000

    Super Trunfo - Países: Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main(){
     char estado1; 
     char codigo1[4], cidade1[50];
     int populacao1, pontos_turisticos1;
-    float area1,PIB1;
+    float area1, PIB1, pib_bilhoes1, pib_em_reais1, densid_populacional1, PIB_per_capita1;
 
 //Importante cadastrar uma carta e imprimir o resultado, pois evita cansar o usuário
     printf("\nBem vindo ao Jogo Super Trunfo - Países!\n");
@@ -27,35 +27,41 @@ int main(){
     printf("Número de habitantes (número inteiro): \n");
     scanf("%d", &populacao1);
 
-    printf("Área da cidade em Km² (separado por ponto): \n");
+    printf("Área da cidade em Km² (com ponto): \n");
     scanf("%f", &area1);
 
-    printf("Qual o PIB? (Separado por ponto)\n");
+    printf("Qual o PIB? (em milhões com ponto)\n");
     scanf("%f", &PIB1);
 
     printf("Há quantos pontos turísticos?\n");
     scanf("%d", &pontos_turisticos1);
 
 //Área para exibição dos dados da cidade 1
+    pib_bilhoes1 = PIB1 / 1000; //Transformar em bilhões de reais
+    densid_populacional1 = populacao1 / area1;
+    pib_em_reais1 = PIB1 * 1000000; //Converter para reais
+    PIB_per_capita1 = pib_em_reais1 / populacao1;
 
-    printf("\nCarta 1\n"); //Aqui sairá o resultado da carta 1
+    printf("\n*** Carta 1 ***\n"); //Aqui sairá o resultado da carta 1
     printf("Estado: %c\n", estado1);
     printf("Código: %c%s\n", estado1, codigo1); //Resposta da Letra do Estado e do número entre 01-04
     printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %f Km²\n", area1);
-    printf("PIB: %f de reais\n", PIB1);
-    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1); //Fim da impressão da carta 1
+    printf("Área: %.2f Km²\n", area1);
+    printf("PIB: %.2f bilhão(ões) de reais\n", pib_bilhoes1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1); 
+    printf("Densidade Populacional: %.2f hab/km²\n", densid_populacional1);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_capita1); //Fim da impressão da carta 1
 
 //Área para definição das variáveis para armazenar as propriedades da próxima cidade
     char estado2; 
-    char codigo2[4], cidade2[50]; 
+    char codigo2[4], cidade2[50];
     int populacao2, pontos_turisticos2;
-    float area2,PIB2;
+    float area2, PIB2, pib_bilhoes2, pib_em_reais2, densid_populacional2, PIB_per_capita2;
 
-    printf("\nVamos cadastrar a Carta 2.\n"); //Área para entrada de dados da carta 1
-    
-    printf("\nEm maiúsculo de 'A' a 'H', digite a letra inicial do Estado: \n");
+    printf("\nVamos cadastrar a Carta 2.\n"); //Área para entrada de dados da carta 2
+
+    printf("Em maiúsculo de 'A' a 'H', digite a letra inicial do Estado: \n");
     scanf(" %c", &estado2);
 
     printf("Escreva um número entre 01 e 04: \n");
@@ -67,25 +73,31 @@ int main(){
     printf("Número de habitantes (número inteiro): \n");
     scanf("%d", &populacao2);
 
-    printf("Área da cidade em Km² (separado por ponto): \n");
+    printf("Área da cidade em Km² (com ponto): \n");
     scanf("%f", &area2);
 
-    printf("Qual o PIB? (Separado por ponto)\n");
+    printf("Qual o PIB? (em milhões com ponto)\n");
     scanf("%f", &PIB2);
 
     printf("Há quantos pontos turísticos?\n");
     scanf("%d", &pontos_turisticos2);
 
-// Área para exibição dos dados da cidade 2
+//Área para exibição dos dados da cidade 2
+    pib_bilhoes2 = PIB2 / 1000; //Transformar em bilhões de reais
+    densid_populacional2 = populacao2 / area2;
+    pib_em_reais2 = PIB2 * 1000000; //Converter para reais
+    PIB_per_capita2 = pib_em_reais2 / populacao2;
 
-    printf("\nCarta 2\n"); // //Aqui sairá o resultado da carta 2
+    printf("\n*** Carta 2 ***\n"); //Aqui sairá o resultado da carta 2
     printf("Estado: %c\n", estado2);
     printf("Código: %c%s\n", estado2, codigo2); //Resposta da Letra do Estado e do número entre 01-04
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área: %f Km²\n", area2);
-    printf("PIB: %f de reais\n", PIB2);
-    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2); //Fim da impressão da carta 2
+    printf("Área: %.2f Km²\n", area2);
+    printf("PIB: %.2f bilhão(ões) de reais\n", pib_bilhoes2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2); 
+    printf("Densidade Populacional: %.2f hab/km²\n", densid_populacional2);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_capita2); //Fim da impressão da carta 2
 
     return 0;
 
